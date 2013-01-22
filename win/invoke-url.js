e28ed7ddfc9a882d2dc57d8e5b42d538311a451d
Aviv Eyal <avivey@gmail.com>
2013-01-22 21:07:36 +0200

    allow dash!

--- win/invoke-url.js
+++ win/invoke-url.js
@@ -1,5 +1,5 @@
 var input = WScript.Arguments.Item(0);
-var match = /^\s*([a-z]+:\/\/[a-zA-Z0-9_\.\/\?&=]+)\s*$/.exec(input);
+var match = /^\s*([a-z]+:\/\/[a-zA-Z0-9_\.\/\?&=-]+)\s*$/.exec(input);
 
 if (match) {
   var shell = new ActiveXObject("WScript.Shell");
