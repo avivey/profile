dc0395f331e080e5204c456768257039373f4007
Aviv Eyal <aviv@clover.com>
2015-05-13 11:22:28 -0700

    whitelist more

--- win/invoke-url.js
+++ win/invoke-url.js
@@ -1,5 +1,5 @@
 var input = WScript.Arguments.Item(0);
-var match = /^\s*([a-z]+:\/\/[a-zA-Z0-9:_\.\/\?$&=-]+)\s*$/.exec(input);
+var match = /^\s*([a-z]+:\/\/[a-zA-Z0-9:_\.\/\?$&=+-]+)\s*$/.exec(input);
 
 if (match) {
   var shell = new ActiveXObject("WScript.Shell");
