160fbdbeb4bce48aeb61d5841d83d358b729a2c7
Aviv Eyal <avivey@gmail.com>
2020-04-24 02:12:50 -0700

    Another script to help refactoring commits

---
+++ bin/split-commit-to-files.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+# Reverts HEAD, then re-applies it one file at at time.
+
+cd $(git rev-parse --show-toplevel)
+set -e
+git diff --quiet HEAD
+
+echo Breaking apart this commit:
+git log -1 --oneline --no-decorate
+
+FILES=$(git diff --name-only HEAD~)
+MESSAGE=$(git log --oneline --no-decorate -1)
+BASE_HASH=$(git log -1 --format=%H)
+
+git revert --no-edit HEAD
+
+for FILE in $FILES
+do
+  git checkout $BASE_HASH -- $FILE
+  git commit -m "$MESSAGE : $FILE"
+done
